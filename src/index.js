de32eb809eda269c428f20bb8bc487719452d130
Tiago Rosa <tiago.tarinfo@gmail.com>
2020-11-03 08:59:40 -0300

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
+const {ApolloServer,gql} = require('apollo-server-express')
 const express = require('express');
 
-const app = express();
+require('dotenv').config();
+const db = require('./db');
+
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
-app.get('/',(req,res)=>res.send('Hello World!!!!'));
+let  notes = [
+  {id: '1', content: 'This is a note', author: 'Adam Scott'},
+  {id: '2', content: 'This is another note', author: 'Harlow Everly'},
+  {id: '3', content: 'Oh hey look, another note!', author: 'Riley harrison'}
+];
 
-app.listen(port, ()=>console.log(`Server running at http://localhost:${port}`));
+const typeDefs = gql`
+  type Note{
+    id: ID!
+    content: String!
+    author: String!
+  },
+  type Query{
+    hello: String
+    notes:[Note!]!
+    note(id: ID!): Note!
+  },
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World',
+    notes: () => notes,
+    note: (parent,args) =>{
+      return notes.find(note=>note.id === args.id);
+    }
+  },
+  Mutation:{
+    newNote: (parent,args) =>{
+      let noteValue = {
+        id:String(notes.length+1),
+        content: args.content,
+        author: "Adam Scott"
+      };
+      notes.push(noteValue);
+      return noteValue
+    }
+  }
+};
+
+const app = express();
+
+db.connect(DB_HOST);
+
+const server = new ApolloServer({typeDefs,resolvers});
+
+server.applyMiddleware({app,path:'/api'});
+
+app.listen({port},()=>console.log(`GraphQl Server running at http://localhost:${port}${server.graphqlPath}`));
+
