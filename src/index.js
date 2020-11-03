ff66b2d3eb34f706337341dd5c4b8e677d579805
Tiago Rosa <tiago.tarinfo@gmail.com>
2020-11-03 09:10:38 -0300

    change query and resolvers to take informations in the database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const {ApolloServer,gql} = require('apollo-server-express')
 const express = require('express');
 
+const models = require('./models');
+
 require('dotenv').config();
 const db = require('./db');
+const { Model } = require('mongoose');
 
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
@@ -32,20 +35,17 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
-    notes: () => notes,
-    note: (parent,args) =>{
-      return notes.find(note=>note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent,args) =>{
+      return await models.Note.findById(args.id);
     }
   },
   Mutation:{
-    newNote: (parent,args) =>{
-      let noteValue = {
-        id:String(notes.length+1),
-        content: args.content,
-        author: "Adam Scott"
-      };
-      notes.push(noteValue);
-      return noteValue
+    newNote: async (parent,args) =>{
+      return await models.Note.create({content: args.content,
+        author: "Adam Scott"});
     }
   }
 };
